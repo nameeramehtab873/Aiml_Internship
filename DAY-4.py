17e2a2a930f3adc4892fbec341d05c5e1ea41a49
= <=>
2026-02-05 12:20:46 +0530

    ryui

---
+++ DAY-4.py
@@ -0,0 +1,15 @@
+student = {
+    "id" : "059",
+    "name" : "nameera",
+    "course":"mca",
+
+}
+print(student)
+student(["city"]) : "dvg"
+print(student["city"])
+student.update({"name":"mehtab"})
+print(student)
+student.pop("course")
+print(student)
+
+
