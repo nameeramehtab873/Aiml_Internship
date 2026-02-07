2bf40221d69345c4f83380f1b299efd70b8a6b8d
= <=>
2026-02-07 19:47:48 +0530

    assigment sat

---
+++ main2.py
@@ -0,0 +1,6 @@
+import math_operations
+result_power=math_operations.power(4,6)
+numbers=[10,20,30,40,50]
+result_avg=math_operations.average(numbers)
+print("4^6=",result_power)
+print("average=",result_avg)
