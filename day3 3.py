ee0a8a491124531bd9d0a2db4100644dc5c84bc3
= <=>
2026-02-08 11:35:48 +0530

    day3 work3

---
+++ day3 3.py
@@ -0,0 +1,8 @@
+fruits=["apple","banana","carrots","dates"]
+print("current fruits",fruits)
+fruits.append("eggs")
+print(fruits)
+fruits.remove("banana")
+print(fruits)
+fruits.sort()
+print("updated fruits:",fruits)
